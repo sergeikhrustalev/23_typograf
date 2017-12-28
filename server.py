448dfd316f47b8271035a8011071ca01249e0aab
Sergei Khrustalev <sergei@khrustalev.net>
2017-12-28 15:17:29 +0300

    first commit

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def form():
-    return render_template('form.html')
+
+    if request.method == 'POST':
+        result_text = request.form.get('text')
+ 
+    return render_template('form.html', result_text=result_text)
 
 if __name__ == "__main__":
     app.run()
